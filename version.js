35376747920c94fa96448b16c30345dad4e93a15
Oscar Polania <opolania11@gmail.com>
2026-07-05 22:23:46 -0500

    Update version.js

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 /* Control de versión del frontend participante. Súbela en cada despliegue
    para forzar la actualización del service worker y la caché. */
-window.APP_VERSION = '3.8.0'; 
+window.APP_VERSION = '3.9.0';
